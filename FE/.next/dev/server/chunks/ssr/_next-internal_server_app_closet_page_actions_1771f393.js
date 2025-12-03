39bf4869d3f1233bb8b8bdd0df07dcb711384245
hun <huney0238@gmail.com>
2025-12-03 21:27:09 +0900

    chore: improve .gitignore for FE-only development

--- FE/.next/dev/server/chunks/ssr/_next-internal_server_app_closet_page_actions_1771f393.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/closet/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_closet_page_actions_1771f393.js.map
--- FE/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"gtYkv1pcxCGIdo39FqV36VyH6JlWmSmN/I5EUR5pUg4=\"\n}"
